f1e85a93c45db4d3d46d9d3b7fc2b2577a897ca5
Jcemer <jeancarloemer@gmail.com>
2012-11-11 18:27:04 -0200

    gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -7,6 +7,6 @@
 cd "$(dirname $0)";
 parent_sha=$(git show-ref -s refs/heads/gh-pages)
 doc_sha=$(git ls-tree -d HEAD public | awk '{print $3}')
-new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p $parent_sha)
+new_commit=$(echo "Auto-update" | git commit-tree $doc_sha -p $parent_sha)
 git update-ref refs/heads/gh-pages $new_commit
 exit;
